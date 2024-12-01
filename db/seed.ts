b6f6de388a16bb16830d6458807fddbde95500a4
Abaranasia <rankirlian@gmail.com>
2024-12-01 20:43:35 +0100

    Add table date seeds

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,34 @@
-import { db } from 'astro:db';
+import {Role, User, db } from 'astro:db';
+import { v4 as UUID } from 'uuid';
+import bcrypt from 'bcryptjs';
 
 // https://astro.build/db/seed
 export default async function seed() {
 	// TODO
+
+	const roles = [
+		{ id: 'admin', name: 'Administrator' },
+		{ id: 'user', name: 'System User' },
+	];
+
+	const johnDoe = {
+		id: UUID(),
+		name: 'John Doe',
+		email: 'john.doe@gmail.com',
+		password: bcrypt.hashSync('123123'),
+		role: 'admin',
+	};
+
+	const janeDoe = {
+		id: UUID(),
+		name: 'Jane Doe',
+		email: 'jane.doe@gmail.com',
+		password: bcrypt.hashSync('123123'),
+		role: 'user',
+	};
+
+	await db.insert(Role).values(roles);
+	await db.insert(User).values([johnDoe, janeDoe]);
+
+
 }
