1d00491ae4e705537ed17fb6e77ddd47c49dbcb1
Abaranasia <rankirlian@gmail.com>
2024-12-01 20:20:07 +0100

    Update database config to define tables

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,28 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb } from 'astro:db';
+import { defineTable } from 'node_modules/@astrojs/db/dist/_internal/runtime/virtual';
+
+const User =defineTable({
+  columns: {
+    id: column.text( { primaryKey: true, unique: true } ),
+    name: column.text(),
+    email: column.text( { unique: true } ),
+    password: column.text(),
+    createdAt: column.date( { default: new Date() } ),
+    role: column.text( { references: () => Role.columns.id} ), // reference to Role table; admin, user, etc
+  }
+});
+
+const Role = defineTable( {
+  columns: {
+    id: column.text( { primaryKey: true, unique: true } ),
+    name: column.text(),
+  }
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {
+    User,
+    Role,
+  }
 });
