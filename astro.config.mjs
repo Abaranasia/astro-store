08a75964c0253787f697af9982d389016988d6d6
Abaranasia <rankirlian@gmail.com>
2024-12-01 20:10:03 +0100

    Updsate and add config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
+
+import auth from 'auth-astro';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), auth()],
+  output: 'server',
+  adapter: netlify()
+});
