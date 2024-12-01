20ad371515d20143a35d79267ca4d59b3d35e401
Abaranasia <rankirlian@gmail.com>
2024-12-01 23:53:37 +0100

    Update to add credentials setup

--- auth.config.mjs
+++ auth.config.mjs
@@ -1,11 +1,36 @@
-import GitHub from '@auth/core/providers/github';
+import { db, User } from 'astro:db';
 import { defineConfig } from 'auth-astro';
+
+import Credentials from '@auth/core/providers/credentials';
+
+import bcrypt from 'bcryptjs';
 
 export default defineConfig({
   providers: [
-    GitHub({
-      clientId: import.meta.env.GITHUB_CLIENT_ID,
-      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
-    }),
+    Credentials({
+      credentials: {
+        email: { label: 'email', type: 'email' },
+        password: { label: 'password', type: 'password' },
+      },
+      authorize: async ({ email, password }) => {
+        const [user] = await db.select().from(User).where(eq(User.email, email));
+
+        if (!user) {
+          throw new Error ('User not found')
+        }
+
+        if (!bcrypt.compareSync(password, user.password )) {
+          throw new Error ('Invalid password')
+        }
+
+        const {password, ...rest} = user; // To remove the password from the return response
+        return rest
+      }
+    })
+    /*     GitHub({
+          clientId: import.meta.env.GITHUB_CLIENT_ID,
+          clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+        }), 
+    */
   ],
 });
