8c727b00cf695a70862577d82fe1f2aa897f4fe7
Abaranasia <rankirlian@gmail.com>
2024-12-22 00:33:45 +0100

    Update format style and fix import

--- src/actions/products/create-update-product.action.ts
+++ src/actions/products/create-update-product.action.ts
@@ -1,17 +1,17 @@
-import { ImageUpload } from "@/utils/image-upload";
+import { ImageTools } from "@/utils/imageTools";
 import { defineAction } from "astro:actions";
 import { db, eq, Product, ProductImage } from "astro:db";
 import { z } from "astro:schema";
 import { getSession } from "auth-astro/server";
 import { v4 as UUID } from "uuid";
 
-const MAX_FILE_SIZE = 5_000_000 // 5MB
+const MAX_FILE_SIZE = 5_000_000; // 5MB
 const ACCEPTED_IMAGE_TYPES = [
-'image/jpeg',
-'image/jpg',
-'image/png',
-'image/webp',
-'image/svg+xml',
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+  "image/svg+xml",
 ];
 
 export const createUpdateProduct = defineAction({
@@ -29,13 +29,13 @@ export const createUpdateProduct = defineAction({
     type: z.string(),
 
     imageFiles: z.array(
-      z.instanceof(File)
-      .refine( file => file.size <= MAX_FILE_SIZE, 'max image file is 5Mb')
-      .refine( file => {
-        if (file.size === 0) return true;
-        return ACCEPTED_IMAGE_TYPES.includes(file.type);
-      }, `Only supported image types are: ${ACCEPTED_IMAGE_TYPES.join(',')}`)
-    ).optional(),
+          z.instanceof(File)
+          .refine((file) => file.size <= MAX_FILE_SIZE, "max image file is 5Mb")
+          .refine((file) => {
+            if (file.size === 0) return true;
+            return ACCEPTED_IMAGE_TYPES.includes(file.type);
+          }, `Only supported image types are: ${ACCEPTED_IMAGE_TYPES.join(",")}`)
+      ).optional(),
 
     // TODO imagen
   }),
@@ -44,7 +44,8 @@ export const createUpdateProduct = defineAction({
     // NTH: middleware to check user permissions
     const user = session?.user;
 
-    if (!user) throw new Error("Unauthorized operation");
+    if (!user || user.role !== "admin")
+      throw new Error("Unauthorized operation");
 
     const { id = UUID(), imageFiles, ...rest } = form;
     rest.slug = rest.slug.toLowerCase().replaceAll(" ", "-").trim();
@@ -59,38 +60,34 @@ export const createUpdateProduct = defineAction({
 
     if (!form.id) {
       // Create new product
-      queries.push(
-        db.insert(Product).values(product)
-      );
+      queries.push(db.insert(Product).values(product));
     } else {
       // Update existing product
-      queries.push(
-        db.update(Product).set(product).where(eq(Product.id, id))
-      );
+      queries.push(db.update(Product).set(product).where(eq(Product.id, id)));
     }
 
     // Image insert
-    const secureUrls:string[] = [];
+    const secureUrls: string[] = [];
     if (
       form.imageFiles &&
       form.imageFiles.length > 0 &&
       form.imageFiles[0].size > 0
     ) {
       const urls = await Promise.all(
-        form.imageFiles.map(file => ImageUpload.upload(file))
+        form.imageFiles.map((file) => ImageTools.upload(file))
       );
 
       secureUrls.push(...urls.filter((url): url is string => url !== null));
-    };
+    }
 
-    secureUrls.forEach( imageUrl => {
+    secureUrls.forEach((imageUrl) => {
       const imageObj = {
         id: UUID(),
-        image:imageUrl,
+        image: imageUrl,
         productId: product.id,
       };
 
-      queries.push( db.insert(ProductImage).values(imageObj))
+      queries.push(db.insert(ProductImage).values(imageObj));
     });
 
     await db.batch(queries);
