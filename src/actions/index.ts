4f9f4df7338dc47f3bc2595fe14a5a189bf92de4
Abaranasia <rankirlian@gmail.com>
2024-12-22 17:37:57 +0100

    Update to add deleteProductImage import

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,7 @@ import { loginUser, logout, registerUser } from './auth';
 import { loadProductsFromCart } from './cart/load-products-from-cart.action';
 import { getproductBySlug, getProductsByPage } from './products';
 import { createUpdateProduct } from './products/create-update-product.action';
+import { deleteProductImage } from './products/delete-product-image.action';
 
 export const server = {
   // actions
@@ -20,4 +21,5 @@ export const server = {
 
   // Admin products
   createUpdateProduct,
+  deleteProductImage,
 };
